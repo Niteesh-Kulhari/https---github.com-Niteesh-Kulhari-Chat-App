ad8a0482a249b446944a2245c5c1caf079cba683
Niteesh Kulhari <nkulhari96@gmail.com>
2024-04-01 14:20:48 -0400

    "added hooks and frontend functionality"

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,13 +1,20 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation"
 
 const Conversations = ()=>{
+
+    const {loading, conversations} = useGetConversations();
+    //console.log("Conversations", conversations)
     return <div py-2 flex-col overflow-auto>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
+        {conversations.map((conversation,idx)=>(
+            <Conversation
+             key={conversation._id}
+             conversation={conversation}
+             emoji={getRandomEmoji()}
+             lastIdx={idx === conversations.length-1}
+            />
+        ))}
     </div>
 }
 
